0ff0f0e70b304ec3edd56ceb8d73b59013476634
shen <santi.dionis@gmail.com>
2023-09-20 21:15:22 +0200

    ADD: TAREAS

--- src/components/Tarea.vue
+++ src/components/Tarea.vue
@@ -1,6 +1,21 @@
 <template>
     <div>
         <h1 class="siaplay-4 text-center">Listado Tareas</h1>
+        <hr>
+        <div class="row">
+            <div class="col-lg-8 offset-lg-2">
+                <div class="card mt-4">
+                    <div class="card body">
+                        <div class="input-group">
+                            <input type="text" class="form-control form-control-lg" placeholder="Agregar Tarea">
+                            <div class="input-group-append"></div>
+
+                                <button class="btn btn-success btn-lg">Agregar</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
 
     </div>
 </template>
