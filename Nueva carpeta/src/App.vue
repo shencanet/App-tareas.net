5ce1d0664d493aa72d5ef08c0ff9bee2dc38a1d2
shen <santi.dionis@gmail.com>
2023-09-24 13:39:29 +0200

    ADD: BUGS

---
+++ Nueva carpeta/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="fondo">
+    <Navbar />
+    <Tarea />
+  </div>
+</template>
+
+<script>
+import Navbar from "./components/Navbar";
+import Tarea from "./components/Tarea";
+
+export default {
+  name: "App",
+  components: {
+    Navbar,
+    Tarea,
+  },
+};
+</script>
+
+<style>
+.fondo {
+  background-image: url("./assets/fondo2.jpg");
+  height: 100vh;
+}
+</style>
